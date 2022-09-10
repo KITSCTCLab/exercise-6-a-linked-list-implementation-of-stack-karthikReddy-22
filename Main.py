fc587ef6e2cf62a6720d8d857dd95c97c77f2bea
karthikReddy-22 <110010639+karthikReddy-22@users.noreply.github.com>
2022-09-10 16:28:25 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,42 @@
+class Node:
+  def _init_(self, data):
+    self.data = data
+    self.next = None
 
+class Stack:
+  def _init_(self):
+    self.head = None
+
+  def push(self, data) -> None:
+    nn = Node(data)
+    nn.next = self.head
+    self.head = nn
+
+  def pop(self) -> None:
+    if(self.head!=None):
+      temp = self.head
+      self.head = temp.next
+  def status(self):
+    if(self.head  != None):
+      temp = self.head
+      while temp.next!= None:
+        print(temp.data, end = "")
+        print("=>", end = "")
+        temp = temp.next
+      print(temp.data, end = "=>")
+      
+    print("None")
+
+# Do not change the following code
+stack = Stack()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "push":
+    stack.push(int(data[i]))
+  elif operations[i] == "pop":
+    stack.pop()
+stack.status()
